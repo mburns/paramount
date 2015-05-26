1a293f081791ffcfb128e3dc070d911a5ee07bc6
Michael Burns <michael.burns@rackspace.com>
2015-05-25 17:36:28 -0700

    refactor test/

--- test/fixtures/cookbooks/test/metadata.rb
+++ test/fixtures/cookbooks/test/metadata.rb
@@ -2,3 +2,5 @@ name 'test'
 license 'Apache 2.0'
 description 'Installs/Configures test'
 version '0.1.0'
+
+depends 'sovereign'
--- test/fixtures/cookbooks/test/recipes/default.rb
+++ test/fixtures/cookbooks/test/recipes/default.rb
@@ -17,3 +17,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+node.set['packages'] = %w(htop iftop iotop iostat lsof netstat tcpdump vmstat)
