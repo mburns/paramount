339c786442b070bf3fd82ffac4e9144382451f01
Michael Burns <michael.burns@rackspace.com>
2015-06-01 03:03:58 -0700

    add *_url attrs to metadata

--- metadata.rb
+++ metadata.rb
@@ -10,6 +10,9 @@ description 'Installs/Configures a Sovereign software stack'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.1'
 
+source_url 'https://github.com/mburns/cookbook-paramount' if respond_to?(:source_url)
+issues_url 'https://github.com/mburns/cookbook-paramount/issues' if respond_to?(:issues_url)
+
 depends 'aide'
 depends 'apt'
 depends 'ark'
