1aca2fe7dc52568f71134292c890bdc29ee86629
Michael Burns <michael@mirwin.net>
2016-10-11 23:07:16 -0700

    remove duplicate code, this is handled by ::_security

--- recipes/_web.rb
+++ recipes/_web.rb
@@ -10,15 +10,4 @@ include_recipe 'paramount::default'
 
 include_recipe 'chef_nginx'
 
-# service 'nginx' do
-#   action %i(enable start)
-# end
-
-openssl_x509 "/etc/httpd/ssl/#{node['paramount']['domain']}.pem" do
-  common_name node['paramount']['domain']
-  org node['paramount']['organization']
-  org_unit node['paramount']['organization_unit']
-  country 'US'
-end
-
 include_recipe 'paramount::wallabag'
