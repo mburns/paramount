ad9ef6bb3b23b4a8fb123381925e74ab5457344a
Michael Burns <michael@mirwin.net>
2016-10-16 01:00:25 -0700

    LDAP server

---
+++ recipes/ldap.rb
@@ -0,0 +1,19 @@
+#
+# Cookbook Name:: paramount
+# Recipe:: ldap
+#
+# Copyright (C) 2016 Michael Burns
+# License:: Apache License, Version 2.0
+#
+
+include_recipe "openldap::auth"
+
+node.default['openldap']['manage_ssl'] = true
+
+# Use https://github.com/atomic-penguin/cookbook-certificate cookbook for advanced certificate deployment
+# or use wrapper cookbook with following code to source ssl files from the wrapper cookbook folder structure:
+# r = resources("cookbook_file[#{node['openldap']['ssl_cert']}]")
+# r.cookbook('NAME OF YOUR WRAPPER COOKBOK')
+#
+# r = resources("cookbook_file[#{node['openldap']['ssl_key']}]")
+# r.cookbook('NAME OF YOUR WRAPPER COOKBOK')
