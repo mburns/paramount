ac5b21124ff929abe2b0198106d02cf398ea888d
Michael Burns <michael@mirwin.net>
2017-04-02 00:01:11 -0700

    fix '== 0' to .zero? logic

--- lib/rake_helper.rb
+++ lib/rake_helper.rb
@@ -4,5 +4,5 @@ ROOT_DIR = File.expand_path('../../', __FILE__)
 
 def abort_on_failure(command, message = '')
   `#{command}`
-  abort(message) unless $CHILD_STATUS.exitstatus == 0
+  abort(message) unless $CHILD_STATUS.exitstatus.zero?
 end
