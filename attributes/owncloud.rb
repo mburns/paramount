e3bad4caaa9d1ef873216eb07a554e9c9be903d2
Michael Burns <michael@mirwin.net>
2017-04-02 03:12:18 -0700

    cleanup

--- attributes/owncloud.rb
+++ attributes/owncloud.rb
@@ -10,6 +10,3 @@ default['owncloud']['server_name'] = 'example.com'
 default['owncloud']['ssl'] = true
 default['owncloud']['config'] = {}
 default['owncloud']['sendfile'] = false
-# default['owncloud']['admin']['pass'] = 'hunter42'
-# default['owncloud']['config']['dbpassword'] = 'hunter42'
-# default['owncloud']['database']['rootpassword'] = 'hunter42'
