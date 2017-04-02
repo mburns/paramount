fe7339060656af6a60a8d572f0251b1c5a0f5cb1
Michael Burns <michael@mirwin.net>
2017-04-01 23:39:43 -0700

    refactor Rake individual tasks into lib/tasks/ files

---
+++ lib/tasks/style.rb
@@ -0,0 +1,44 @@
+require 'rubocop/rake_task'
+require 'jsonlint/rake_task'
+require 'yamllint/rake_task'
+require 'foodcritic'
+
+require_relative '../rake_helper'
+
+namespace :style do
+  desc 'Lint ERB templates'
+  task :erblint do
+    puts 'Syntax checking ERB templates...'
+    Dir.glob('**/*.erb').each do |f|
+      next if f.start_with?('cookbooks/', 'vendor/') # skip vendored things
+      abort_on_failure("erb -x -T '-' #{f} | ruby -c", "** Failed: #{f}")
+    end
+  end
+
+  YamlLint::RakeTask.new do |t|
+    t.paths = %w(
+      **/.*.{yml,yaml}
+      **/*.{yml,yaml}
+    )
+  end
+
+  JsonLint::RakeTask.new do |t|
+    t.paths = %w(
+      **/*.json
+    )
+  end
+
+  FoodCritic::Rake::LintTask.new do |t|
+    t.options = {
+      fail_tags: ['any'],
+      cookbook_paths: ['site-cookbooks', 'test/fixtures/cookbooks'],
+      tags: [
+      ]
+    }
+  end
+
+  RuboCop::RakeTask.new
+
+  desc 'Run all linters'
+  task all: [:erblint, :jsonlint, :yamllint, :rubocop, :foodcritic]
+end
