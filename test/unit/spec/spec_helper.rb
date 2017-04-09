f713b62a27af6837af7139fada2d1a465e194f5e
Michael Burns <michael@mirwin.net>
2017-04-08 23:19:31 -0700

    lint

--- test/unit/spec/spec_helper.rb
+++ test/unit/spec/spec_helper.rb
@@ -32,7 +32,7 @@ def stub_resources
   stub_command('which nginx').and_return '/usr/sbin/nginx'
 
   stub_command('test -d /etc/php5/fpm/pool.d || mkdir -p /etc/php5/fpm/pool.d').and_return false
-  
+
   stub_command('ls /var/lib/postgresql/9.3/main/recovery.conf').and_return true
 
   stub_command("psql -c 'SELECT lanname FROM pg_catalog.pg_language' postfix | grep '^ plpgsql$'").and_return true
