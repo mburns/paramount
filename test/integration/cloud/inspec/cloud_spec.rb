5a100f36461e7b1e657ab8435fc9722ecab85d64
Michael Burns <michael@mirwin.net>
2018-03-31 22:11:12 -0700

    Fix spec_helper path for integration tests

--- test/integration/cloud/inspec/cloud_spec.rb
+++ test/integration/cloud/inspec/cloud_spec.rb
@@ -1,4 +1,4 @@
 # License:: Apache License, Version 2.0
 #
 
-require_relative '../spec_helper'
+require_relative '../../spec_helper'
--- test/integration/default/inspec/security_spec.rb
+++ test/integration/default/inspec/security_spec.rb
@@ -1,7 +1,7 @@
 # License:: Apache License, Version 2.0
 #
 
-require_relative '../spec_helper'
+require_relative '../../spec_helper'
 
 describe selinux do
   it { should be_permissive }
